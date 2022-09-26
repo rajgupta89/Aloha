ec20dcbc966ad36189be00d70e8c0ff59d953b2b
rajgupta89 <gupta77raj123@gmail.com>
2022-09-26 12:37:40 +0530

    commit

--- string_palindrome.java
+++ string_palindrome.java
@@ -4,12 +4,14 @@ public class string_palindrome {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         String s=in.next();
-        String s1="";
-        for (int i = 0; i < s.length(); i++) {
-            s1=s.charAt(i)+s1;
-        }
+        // String s1="";
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        // for (int i = 0; i < s.length(); i++) {
+        //     s1=s.charAt(i)+s1;
+        // }
         // System.out.println(s1);
-        if(s.equals(s1))
+        if(s.equals(sb.toString()))
           System.out.println("palindrome");
         else
           System.out.println("not palindrome");
